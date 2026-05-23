9a91517f5321edc41b17b31a00caa9e98ce87748
Hoonjaa <gnswp8978@gmail.com>
2026-05-23 18:49:21 +0900

    잘못했당

--- 3D_Maze_Shooting_Game/3D_Maze_Shooting_Game/3D_Maze_Shooting_Game.cpp
+++ 3D_Maze_Shooting_Game/3D_Maze_Shooting_Game/3D_Maze_Shooting_Game.cpp
@@ -1,7 +1,7 @@
 ﻿// 3D_Maze_Shooting_Game.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
 
-#include "framework.h"
+#include "stdafx.h"
 #include "3D_Maze_Shooting_Game.h"
 
 #define MAX_LOADSTRING 100
@@ -43,14 +43,23 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     MSG msg;
 
     // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
+    while (1)
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        if (::PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
         {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
+            if (msg.message == WM_QUIT) break;
+            if (!::TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                ::TranslateMessage(&msg);
+                ::DispatchMessage(&msg);
+            }
+        }
+        else
+        {
+            //Core::Instance()->Progress();
         }
     }
+    //Core::Instance()->OnDestroy();
 
     return (int) msg.wParam;
 }
